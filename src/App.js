9befde69b72e3587731648eff483a9307e787116
ninenpn <pninekub@gmail.com>
2024-04-11 23:16:04 +0300

    init + frontend setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Button, Flex } from 'antd';
+import './stylesheets/alignments.css';
+import './stylesheets/custom-components.css';
+import './stylesheets/form-elements.css';
+import './stylesheets/text-elements.css';
+import './stylesheets/theme.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-primary h-screen flex items-center justify-center'>
+      <h1>
+        SSSF Wallet
+      </h1>
+      <Button type="primary">Primary Button</Button>
     </div>
   );
 }
