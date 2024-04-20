6debac2eacaf0efc522ee35cd1ee87b6d3e18181
ninenpn <pninekub@gmail.com>
2024-04-20 15:17:30 +0300

    TransactionFront

--- client/src/pages/transactions/index.js
+++ client/src/pages/transactions/index.js
@@ -1,8 +1,43 @@
 import React from "react";
+import PageTitle from "../../components/PageTitle";
+import { Table } from "antd";
 
 function Transactions() {
+    const columns = [
+        {
+            title: 'Date',
+            dataIndex: 'date',
+        },
+        {
+            title: 'Amount',
+            dataIndex: 'amount',
+        },
+        {
+            title: 'Type',
+            dataIndex: 'type',
+        },
+        {
+            title: 'Reference',
+            dataIndex: 'reference',
+        },
+        {
+            title: 'Status',
+            dataIndex: 'status',
+        },
+    ]
     return (
-        <div>Transactions</div>
+    <div>
+        <div className="flex justify-between items-center">
+            <PageTitle title="Transactions" />
+
+            <div className="flex gap-1">
+                <button className="primary-outlined-btn">Deposit</button>
+                <button className="primary-contained-btn">Transfer</button>
+            </div>
+        </div>
+
+            <Table columns={columns} dataSource={[]} className="mt-2"/>
+    </div>
     );
 }
 
